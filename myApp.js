41847b574ccec5818f65b205a6cbd5f3ff67ebdd
Wisdom Eleeza <whiy07@gmail.com>
2023-11-05 16:26:23 +0000

    add createManyPeople with callback function

--- myApp.js
+++ myApp.js
@@ -34,7 +34,13 @@ newPerson.save((err, data) => {
 };
 
 const createManyPeople = (arrayOfPeople, done) => {
-  done(null /*, data*/);
+  Person.create(arrayOfPeople, (err, data) => {
+    if(err) {
+      done(err)
+    } else {
+      done(null, data)
+    }
+  })
 };
 
 const findPeopleByName = (personName, done) => {
