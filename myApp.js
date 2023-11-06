a4e8e2b8d4729715c0202784682e17562b245c99
Wisdom Eleeza <whiy07@gmail.com>
2023-11-06 15:41:29 +0000

    add findEditAndSave function with callback(5)

--- myApp.js
+++ myApp.js
@@ -73,23 +73,21 @@ const findPersonById = (personId, done) => {
   })
 };
 
-const findEditThenSave = (personId, person) => {
-  const foodToAdd = "hamburger";
-  
-  Person.findById({ _id: personId}, (err, person) => {
-    if(err) {
-      done(err)
-    } else {
-      person.favoriteFoods.push(foodToAdd)
+const findEditThenSave = (personId, done) => {
+  const foodToAdd = 'hamburger';
 
-      person.save((err, updatePerson) => {
-        if(err){
-          done(err)
-        } else {
-          done(null, updatePerson)
-        }
-      })
-    }
+  // .findById() method to find a person by _id with the parameter personId as search key. 
+  Person.findById(personId, (err, person) => {
+    if(err) return console.log(err); 
+  
+    // Array.push() method to add "hamburger" to the list of the person's favoriteFoods
+    person.favoriteFoods.push(foodToAdd);
+
+    // and inside the find callback - save() the updated Person.
+    person.save((err, updatedPerson) => {
+      if(err) return console.log(err);
+      done(null, updatedPerson)
+    })
   })
 };
 
